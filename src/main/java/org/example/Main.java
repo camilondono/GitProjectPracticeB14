8b0f3b8f3793e33491654c2a1346ed6ee7b8d730
camilondono <camilondono1216@gmail.com>
2022-12-17 10:58:50 -0500

    version2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("First version is completed");
+        System.out.println("version is completed");
     }
 }
